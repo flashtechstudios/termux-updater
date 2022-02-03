5888c390eec766710df6d48aa29fcce3826a3dda
Devesh3293 <96462198+Devesh3293@users.noreply.github.com>
2022-02-03 15:01:14 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 echo I AM NOT RESPONSIBLE FOR ANY DAMAGE CAUSED BY RUNNING THIS SCRIPT. USE AT YOUR OWN RISK!
 echo ==============================
-echo LINUX PACKAGE UPDATER 
+echo TERMUX PACKAGE UPDATER 
 echo ==============================
-echo This script can only update Debian, ubuntu or distros based on them which use APT package manager
+echo This script updates packages on termux
 echo ==============================
 echo Updating....
 pkg upgrade
